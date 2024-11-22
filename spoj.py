01ab87f0ef8815d81a04650d814e33f5f161320f
Jack Batsukh <bayarjavkhlanbatsukh@gmail.com>
2024-11-22 15:48:28 +0800

    weather

---
+++ spoj.py
@@ -0,0 +1,18 @@
+# # #palindrom too ese
+
+# def palindrome(x):
+#     orig = x 
+#     reversed = 0
+#     while x > 0:
+#         digit = x % 10
+#         reversed = reversed * 10 + digit
+#         x = x // 10
+#     if orig == reversed:
+#         return  "true"
+#     else:
+#         return "false"
+
+
+# x = palindrome(int(input("x= ")))
+# print(x)
+
